a71766d809ef5e06dbb90dc1bd7810a5ecb68372
Rohan Das <mailtorohan.97@gmail.com>
2024-07-19 22:29:10 +0530

    Add files via upload

---
+++ groq/app.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import os
+from langchain_groq import ChatGroq
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_community.embeddings import OllamaEmbeddings
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.prompts import ChatPromptTemplate
+from langchain.chains import create_retrieval_chain
+from langchain_community.vectorstores import FAISS
+import time
+
+from dotenv import load_dotenv
+load_dotenv()
+
+## load the Groq API key
+groq_api_key=os.environ['GROQ_API_KEY']
+
+print(groq_api_key)
