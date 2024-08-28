574a829b5d535df34a37c8a71eb5b8cc38076758
Rohan Das <mailtorohan.97@gmail.com>
2024-08-28 15:51:35 +0530

    Update main.py

--- Python/main.py
+++ Python/main.py
@@ -1,6 +1,11 @@
 
-
-
+---------------------------------------------
+SELECT manufacturer,drug, units_sold
+FROM pharmacy_sales
+WHERE
+manufacturer IN ('Roche', 'Bayer', 'AstraZeneca')
+AND
+units_sold NOT BETWEEN 55000 AND 550000;
 ----------------------------------------------
 SELECT manufacturer, drug, units_sold
 FROM pharmacy_sales
