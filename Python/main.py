3e063b59180d100cc93fb843b00ae0d90137c208
Rohan Das <mailtorohan.97@gmail.com>
2024-08-28 11:57:17 +0530

    Update main.py

--- Python/main.py
+++ Python/main.py
@@ -1,5 +1,11 @@
 
 
+--------------------------------
+def factorial(n):
+  if n==0 or n == 1:
+    return 1
+  else:
+    return n*factorial(n-1)
 ----------------------------------
 def m(l,x):
   for i in range(len(l)):
