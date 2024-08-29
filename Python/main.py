83c182b7a81728834bbb36a2b8ef746fa15029d7
Rohan Das <mailtorohan.97@gmail.com>
2024-08-29 09:53:58 +0530

    Update main.py

--- Python/main.py
+++ Python/main.py
@@ -1,8 +1,13 @@
 
 
 
-
-
+------------------------------------------
+def fizz_buzz_sum(target):
+  sum = 0
+  for i in range(target):
+    if (i % 3 == 0) or (i % 5 == 0):
+      sum += i
+  return sum
 ---------------------------------------------
 from math import pi
 class C:
