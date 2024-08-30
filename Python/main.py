5482e7c2e50a83dca1d9f8499d0439d66fb8d7b3
Rohan Das <mailtorohan.97@gmail.com>
2024-08-30 09:44:06 +0530

    Update main.py

--- Python/main.py
+++ Python/main.py
@@ -5,7 +5,11 @@
 
 
 
-
+--------------------------------------------------
+SELECT  drug, CEIL(total_sales/ units_sold) as unit_cost
+FROM pharmacy_sales
+WHERE manufacturer = 'Merck'
+ORDER BY unit_cost
 ----------------------------------------------------
 SELECT ticker, COUNT(ticker) FROM stock_prices
 WHERE ((close - open)/open > 0.1) OR ((close - open)/open < -0.1)
