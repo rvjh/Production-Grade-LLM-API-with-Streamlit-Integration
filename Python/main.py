9db523edf86021bd3c02e4eee3520465bdbf78aa
Rohan Das <mailtorohan.97@gmail.com>
2024-09-02 18:12:13 +0530

    Update main.py

--- Python/main.py
+++ Python/main.py
@@ -11,6 +11,23 @@
 
 
 
+-------------------------------------------------------------
+WITH ranked_measurements AS (
+  SELECT 
+    CAST(measurement_time AS DATE) AS measurement_day, 
+    measurement_value, 
+    ROW_NUMBER() OVER (
+      PARTITION BY CAST(measurement_time AS DATE) 
+      ORDER BY measurement_time) AS measurement_num 
+  FROM measurements
+) 
+
+SELECT 
+  measurement_day, 
+  SUM(measurement_value) FILTER (WHERE measurement_num % 2 != 0) AS odd_sum, 
+  SUM(measurement_value) FILTER (WHERE measurement_num % 2 = 0) AS even_sum 
+FROM ranked_measurements
+GROUP BY measurement_day;
 ------------------------------------------------------
 WITH latest_transactions_cte AS (
   SELECT 
