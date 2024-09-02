06ee93c9a502936a1efd86c0ed08633f4c9adefb
Rohan Das <mailtorohan.97@gmail.com>
2024-09-02 18:00:22 +0530

    Update main.py

--- Python/main.py
+++ Python/main.py
@@ -3,6 +3,32 @@
 
 
 
+
+
+
+
+
+
+
+
+----------------------------------------------------------
+WITH top_10_cte AS (
+  SELECT 
+    artists.artist_name,
+    DENSE_RANK() OVER (
+      ORDER BY COUNT(songs.song_id) DESC) AS artist_rank
+  FROM artists
+  INNER JOIN songs
+    ON artists.artist_id = songs.artist_id
+  INNER JOIN global_song_rank AS ranking
+    ON songs.song_id = ranking.song_id
+  WHERE ranking.rank <= 10
+  GROUP BY artists.artist_name
+)
+
+SELECT artist_name, artist_rank
+FROM top_10_cte
+WHERE artist_rank <= 5;
 -------------------------------
 def m(a,b):
   return list(map(lambda x,y: x+y, a,b)), list(map(lambda x,y: x-y, a,b))
