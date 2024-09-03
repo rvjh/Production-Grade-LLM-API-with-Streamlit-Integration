0557acba26bd25cc34c5208f3a9ed5aff12973da
Rohan Das <mailtorohan.97@gmail.com>
2024-09-03 09:49:15 +0530

    Update main.py

--- Python/main.py
+++ Python/main.py
@@ -11,6 +11,12 @@
 
 
 
+-----------------------------------------------------
+def m(l):
+  return list(filter(lambda x: x%2==0,l))
+
+
+m([1,3,4,5,6,7,8,9])
 -----------------------------------------------------------------
 class Solution:
     def removeDuplicateLetters(self, s: str) -> str:
