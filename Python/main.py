60cf988908cb2183a0353802a566f92127c37e61
Rohan Das <mailtorohan.97@gmail.com>
2024-09-18 09:36:22 +0530

    Update main.py

--- Python/main.py
+++ Python/main.py
@@ -9,7 +9,11 @@
 
 
 
+my_list = [10, 10.5, 20, 30, 'Python', 'Java', 'Ruby']
 
+my_slice = my_list[3:6]
+
+print(my_slice)
 --------------------------------------------------------
 my_list = [10, 10.5, 20, 30, 'Python', 'Java', 'Ruby']
 
