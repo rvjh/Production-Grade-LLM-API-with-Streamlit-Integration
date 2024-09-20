027cca29dde0a2d46362f17cb7a21f07e91b64f7
Rohan Das <mailtorohan.97@gmail.com>
2024-09-20 09:29:04 +0530

    Update main.py

--- Python/main.py
+++ Python/main.py
@@ -10,6 +10,12 @@
 
 
 
+-------------------------------------------------------
+my_tup = ("Romania", "Poland", "Estonia", "Bulgaria", "Slovakia", "Slovenia", "Hungary")
+
+my_slice = my_tup[:-3]
+
+print(my_slice)
 ------------------------------------------------------------------------
 my_tup = ("Romania", "Poland", "Estonia", "Bulgaria", "Slovakia", "Slovenia", "Hungary")
 
