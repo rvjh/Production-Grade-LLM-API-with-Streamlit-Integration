ac09f7d37e62518e41e329a824cf6d578efa364b
Rohan Das <mailtorohan.97@gmail.com>
2024-09-20 09:26:22 +0530

    Update main.py

--- Python/main.py
+++ Python/main.py
@@ -10,6 +10,14 @@
 
 
 
+
+
+---------------------------------------------------------
+my_tup = ("Romania", "Poland", "Estonia", "Bulgaria", "Slovakia", "Slovenia", "Hungary")
+
+last = max(my_tup)
+
+print(last) #should return Slovenia
 ------------------------------------------------------------
 my_tup = ("Romania", "Poland", "Estonia")
 
