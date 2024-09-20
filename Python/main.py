b0dfb225615235f9ec68bead3f788cb0bbbfe73e
Rohan Das <mailtorohan.97@gmail.com>
2024-09-20 09:22:52 +0530

    Update main.py

--- Python/main.py
+++ Python/main.py
@@ -10,8 +10,12 @@
 
 
 
+-----------------------------------------------------
+my_tup = ("Romania", "Poland", "Estonia", "Bulgaria", "Slovakia", "Slovenia", "Hungary")
 
+number = len(my_tup)
 
+print(number)
 ----------------------------------------------------------------
 my_set1 = {1, 4, 6, 5, 9, 0, 8, 3, 2, 7, 11}
 my_set2 = {12, 9, 4, 2, 0, 6}
