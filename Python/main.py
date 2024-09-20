bff7f0742dc91d383c42dc3eb814b909e73ce340
Rohan Das <mailtorohan.97@gmail.com>
2024-09-20 09:23:35 +0530

    Update main.py

--- Python/main.py
+++ Python/main.py
@@ -8,8 +8,12 @@
 
 
 
+--------------------------------------------
+my_tup = ("Romania", "Poland", "Estonia", "Bulgaria", "Slovakia", "Slovenia", "Hungary")
 
+index = my_tup.index('Slovakia')
 
+print(index)
 -----------------------------------------------------
 my_tup = ("Romania", "Poland", "Estonia", "Bulgaria", "Slovakia", "Slovenia", "Hungary")
 
