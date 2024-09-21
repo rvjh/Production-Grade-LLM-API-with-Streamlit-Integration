1a1499b18965b922f90669f7749e8c106232d98f
Rohan Das <mailtorohan.97@gmail.com>
2024-09-21 09:46:38 +0530

    Update main.py

--- Python/main.py
+++ Python/main.py
@@ -8,8 +8,12 @@
 
 
 
+===================
+crypto = {1: "Bitcoin", 2: "Ethereum", 3: "Litecoin", 4: "Stellar", 5: "XRP"}
 
+add = sum(crypto)
 
+print(add)
 ---------------------------------
 crypto = {1: "Bitcoin", 2: "Ethereum", 3: "Litecoin", 4: "Stellar", 5: "XRP"}
 
