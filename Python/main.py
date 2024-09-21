1c3c1469dd7f7c0d8c701aaf79a4fbb951fba529
Rohan Das <mailtorohan.97@gmail.com>
2024-09-21 09:42:59 +0530

    Update main.py

--- Python/main.py
+++ Python/main.py
@@ -7,6 +7,14 @@
 
 
 
+
+
+--------------------
+crypto = {1: "Bitcoin", 2: "Ethereum", 3: "Litecoin", 4: "Stellar", 5: "XRP"}
+
+check = 7 not in crypto
+
+print(check)
 -----------------------------
 crypto = {1: "Bitcoin", 2: "Ethereum", 3: "Litecoin", 4: "Stellar", 5: "XRP"}
 
