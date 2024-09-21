e5e494cb5a280dbd0cf5bb25578c9818b8d7911e
Rohan Das <mailtorohan.97@gmail.com>
2024-09-21 09:38:57 +0530

    Update main.py

--- Python/main.py
+++ Python/main.py
@@ -8,6 +8,13 @@
 
 
 
+
+-----------------------
+crypto = {1: "Bitcoin", 2: "Ethereum", 3: "Litecoin", 4: "Stellar", 5: "XRP"}
+
+crypto.update({6: "Monero"})
+
+print(crypto[6])
 ----------------------------------
 crypto = {1: "Bitcoin", 2: "Ethereum", 3: "Litecoin", 4: "Stellar", 5: "XRP"}
 
