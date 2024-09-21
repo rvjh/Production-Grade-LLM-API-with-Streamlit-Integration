c46d59e7a1c6b46cd4521bf9fbec383b6d89384f
Rohan Das <mailtorohan.97@gmail.com>
2024-09-21 09:39:43 +0530

    Update main.py

--- Python/main.py
+++ Python/main.py
@@ -7,8 +7,12 @@
 
 
 
+------------
+crypto = {1: "Bitcoin", 2: "Ethereum", 3: "Litecoin", 4: "Stellar", 5: "XRP"}
 
+number = len(crypto)
 
+print(number)
 -----------------------
 crypto = {1: "Bitcoin", 2: "Ethereum", 3: "Litecoin", 4: "Stellar", 5: "XRP"}
 
