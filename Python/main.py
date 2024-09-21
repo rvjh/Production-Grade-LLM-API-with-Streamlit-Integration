9b360271493861808e9756c781375d49a63fe68b
Rohan Das <mailtorohan.97@gmail.com>
2024-09-21 09:48:01 +0530

    Update main.py

--- Python/main.py
+++ Python/main.py
@@ -9,6 +9,12 @@
 
 
 
+-------------------------
+crypto = {1: "Bitcoin", 2: "Ethereum", 3: "Litecoin", 4: "Stellar", 5: "XRP"}
+
+key = min(crypto)
+
+print(key)
 -----------------------------
 crypto = {1: "Bitcoin", 2: "Ethereum", 3: "Litecoin", 4: "Stellar", 5: "XRP"}
 
