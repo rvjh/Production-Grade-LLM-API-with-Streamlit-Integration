3bf225f32823caf539faecd2c39e00f6a378784f
Rohan Das <mailtorohan.97@gmail.com>
2024-09-23 09:33:20 +0530

    Update main.py

--- Python/main.py
+++ Python/main.py
@@ -22,6 +22,14 @@
 
 
 
+
+-------------------------------------
+x = "The days of Python 2 are almost over. Python 3 is the king now."
+
+if x[-3:].isdigit():
+    print("True!")
+else:
+    print("False!")
 ----------------------------------------
 x = "The days of Python 2 are almost over. Python 3 is the king now."
 
