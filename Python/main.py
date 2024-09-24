6920ae3c6600f7872c93c14a1805c0ba2fa16f32
Rohan Das <mailtorohan.97@gmail.com>
2024-09-24 12:57:59 +0530

    Update main.py

--- Python/main.py
+++ Python/main.py
@@ -3,6 +3,67 @@
 
 
 
+
+-------------------------------
+# Stack implementation
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class Stack:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.top = new_node
+        self.height = 1
+
+    def print_stack(self):
+        temp = self.top
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+
+    def push_stack(self, value):
+      if self.height == 0:
+        return None
+      else:
+        new_node = Node(value)
+        new_node.next = self.top
+        self.top = new_node
+        self.height += 1
+        return True
+    
+    def pop_stack(self):
+      if self.height == 0:
+        return None
+      else:
+        temp = self.top
+        self.top = self.top.next
+        temp.next = None
+        self.height -= 1
+        return temp.value
+
+
+# Create a stack and push values
+s1 = Stack(1)
+s1.push_stack(2)
+s1.push_stack(3)
+s1.push_stack(4)
+s1.push_stack(5)
+s1.push_stack(6)
+s1.push_stack(7)
+s1.push_stack(8)
+s1.push_stack(9)
+s1.push_stack(10)
+
+# Print all the items in the stack
+s1.print_stack()
+
+s1.pop_stack()
+s1.pop_stack()
+
+s1.print_stack()
 --------------------------------------
 x = [115, 115.9, 116.01, 109, 115, 119.5, ["length", "width", "height"]]
 
