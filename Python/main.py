3f6508f73499cf782165d871b333497aa517f47c
Rohan Das <mailtorohan.97@gmail.com>
2024-09-24 09:18:52 +0530

    Update main.py

--- Python/main.py
+++ Python/main.py
@@ -3,8 +3,13 @@
 
 
 
+---------------------------------
+x = [115, 115.9, 116.01, ["length", "width", "height"], 109, 115, 119.5, ["length", "width", "height"]]
 
-
+if x[3][1].endswith("h") and x[7][0].endswith("h"):
+    print("True!")
+else:
+    print("False!")
 -------------------------------
 if len(x) >= 8 and type(x[6]) is float:
     print("True!")
