a9472e7656dae4ba96c32b9b7150e09d221568dc
Rohan Das <mailtorohan.97@gmail.com>
2024-09-25 09:34:58 +0530

    Update main.py

--- Python/main.py
+++ Python/main.py
@@ -12,6 +12,13 @@
 
 
 
+
+------------------------------------------------
+import pandas as pd
+
+def concatenateTables(df1: pd.DataFrame, df2: pd.DataFrame) -> pd.DataFrame:
+    return pd.concat([df1, df2], ignore_index=True)
+    
 ------------------------------------------------
 import pandas as pd
 
