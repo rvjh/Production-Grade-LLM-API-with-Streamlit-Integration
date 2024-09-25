e4fea7e986e46d2483550bf0d7b74db950bc76b4
Rohan Das <mailtorohan.97@gmail.com>
2024-09-25 09:17:20 +0530

    Update main.py

--- Python/main.py
+++ Python/main.py
@@ -3,6 +3,12 @@
 
 
 
+-----------------------------
+import pandas as pd
+
+def selectFirstRows(employees: pd.DataFrame) -> pd.DataFrame:
+    return employees.head(3)
+    
 ---------------------------
 import pandas as pd
 
