466256bdf2eb8564e317471382e539f8fe5ddfe0
Rohan Das <mailtorohan.97@gmail.com>
2024-09-25 09:25:38 +0530

    Update main.py

--- Python/main.py
+++ Python/main.py
@@ -9,6 +9,14 @@
 
 
 
+
+-----------------------------------------
+import pandas as pd
+
+def modifySalaryColumn(employees: pd.DataFrame) -> pd.DataFrame:
+    employees['salary'] = employees['salary']*2
+    return employees
+    
 --------------------------------------------
 import pandas as pd
 
