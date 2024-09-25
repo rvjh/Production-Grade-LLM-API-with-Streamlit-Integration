aa78a5e30775e7c424c48f67d77eca45daf7f2ef
Rohan Das <mailtorohan.97@gmail.com>
2024-09-25 09:32:58 +0530

    Update main.py

--- Python/main.py
+++ Python/main.py
@@ -12,8 +12,13 @@
 
 
 
+------------------------------------------------
+import pandas as pd
 
-
+def fillMissingValues(products: pd.DataFrame) -> pd.DataFrame:
+    products['quantity'] = products['quantity'].fillna(0)
+    return products
+    
 ---------------------------------------
 import pandas as pd
 
