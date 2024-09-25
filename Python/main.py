492935c905c3ab10241dfd6d7d861d8c6760a7ee
Rohan Das <mailtorohan.97@gmail.com>
2024-09-25 09:29:53 +0530

    Update main.py

--- Python/main.py
+++ Python/main.py
@@ -10,6 +10,20 @@
 
 
 
+
+
+
+--------------------------------------------------------
+import pandas as pd
+
+def renameColumns(students: pd.DataFrame) -> pd.DataFrame:
+    dict={
+        'id':'student_id',
+        'first':'first_name',
+        'last':'last_name',
+        'age':'age_in_years'}
+    return students.rename(columns=dict)
+    
 -----------------------------------------
 import pandas as pd
 
