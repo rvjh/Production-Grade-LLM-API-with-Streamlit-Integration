6dd73b76e57caeec20b5cb97b9ddf2897a327c63
Rohan Das <mailtorohan.97@gmail.com>
2024-09-25 09:23:27 +0530

    Update main.py

--- Python/main.py
+++ Python/main.py
@@ -6,6 +6,14 @@
 
 
 
+
+-----------------------------------------
+import pandas as pd
+
+def dropDuplicateEmails(customers: pd.DataFrame) -> pd.DataFrame:
+    customers = customers.drop_duplicates(subset='email', keep='first')
+    return customers
+    
 -------------------------------------------
 import pandas as pd
 
