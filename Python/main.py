2d74ed73775d6c91a65ce5270aa515962883e4ff
Rohan Das <mailtorohan.97@gmail.com>
2024-09-26 18:26:20 +0530

    Update main.py

--- Python/main.py
+++ Python/main.py
@@ -5,7 +5,53 @@
 
 
 
+---------------------------------------------------------
+## class hashtable
 
+class HashTable:
+  def __init__(self, size=7):
+    self.data_map = [None]*size
+
+  def _hash(self,key):
+    my_hash = 0
+    for letter in key:
+      my_hash = (my_hash + ord(letter)*23)%len(self.data_map)
+    return my_hash
+
+  def print_table(self):
+    for i, val in enumerate(self.data_map):
+      print(f"{i} -> {val}")
+
+  def set_item(self, key, value):
+    index = self._hash(key)
+    if self.data_map[index] == None:
+      self.data_map[index] = []
+    self.data_map[index].append([key, value])
+  
+  def get_item(self, key):
+    index = self._hash(key)
+    if self.data_map[index] is not None:
+      for i in range(len(self.data_map[index])):
+        if self.data_map[index][i][0] == key:
+          return self.data_map[index][i][1]
+    return None
+  
+  def keys(self):
+    all_keys = []
+    for i in range(len(self.data_map)):
+      if self.data_map[i] is not None:
+        for j in range(len(self.data_map[i])):
+          all_keys.append(self.data_map[i][j][0])
+    return all_keys
+
+
+h1 = HashTable()
+h1.set_item('bolts',1400)
+h1.set_item('washers',50)
+h1.set_item('lumber',70)
+h1.print_table()
+h1.get_item('bolts')
+h1.keys()
 
 ---------------------------------------------
 ## class hashtable
