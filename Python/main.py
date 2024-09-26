424a50edf5eee51b6dfe0a0bc4fbd1ea9adc6d6e
Rohan Das <mailtorohan.97@gmail.com>
2024-09-26 16:48:51 +0530

    Update main.py

--- Python/main.py
+++ Python/main.py
@@ -6,6 +6,26 @@
 
 
 
+
+---------------------------------------------
+## class hashtable
+
+class HashTable:
+  def __init__(self, size=7):
+    self.data_map = [None]*size
+
+  def _hash(self,key):
+    my_hash = 0
+    for letter in key:
+      my_hash = (my_hash + ord(letter)*23)%len(self.data_map)
+    return my_hash
+
+  def print_table(self):
+    for i, val in enumerate(self.data_map):
+      print(f"{i} -> {val}")
+
+h1 = HashTable()
+h1.print_table()
 ----------------------------------------------------------------------
 import pandas as pd
 
