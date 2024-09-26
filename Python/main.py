18905e8ebd5a1075264bf1e5eed197d1cd5527a7
Rohan Das <mailtorohan.97@gmail.com>
2024-09-26 14:23:48 +0530

    Update main.py

--- Python/main.py
+++ Python/main.py
@@ -10,8 +10,18 @@
 
 
 
+---------------------------------------------------------
+import pandas as pd
 
-
+def meltTable(report: pd.DataFrame) -> pd.DataFrame:
+    df = report.melt(
+        id_vars = ['product'],
+        value_vars = ['quarter_1','quarter_2','quarter_3','quarter_4'],
+        var_name = 'quarter',
+        value_name = 'sales'
+    )
+    return df
+    
 --------------------------------------------------
 import pandas as pd
 
