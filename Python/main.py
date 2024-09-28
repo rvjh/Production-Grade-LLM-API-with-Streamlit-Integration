73954bb5dca7e7c3f77f3cce8df65fd78d35a1ff
Rohan Das <mailtorohan.97@gmail.com>
2024-09-28 09:07:15 +0530

    Update main.py

--- Python/main.py
+++ Python/main.py
@@ -1,5 +1,26 @@
 
 
+
+
+
+---------------------------------
+def m(n):
+  l = [0,1]
+  if n==0:
+    return l[0]
+  elif n==1:
+    return l
+  else:
+    a = 0
+    b = 1
+    for i in range(2, n):
+      c = a + b
+      l.append(c)
+      a = b
+      b = c
+  return l
+
+m(10)
 ---------------------------------
 def m(s):
   d = {}
