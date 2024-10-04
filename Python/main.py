50b82debfe037100689e91ade56c478caf592fb8
Rohan Das <mailtorohan.97@gmail.com>
2024-10-04 09:32:20 +0530

    Update main.py

--- Python/main.py
+++ Python/main.py
@@ -6,8 +6,24 @@
 
 
 
+-------------------------------------------------
+def f(n):
+  l=[0,1]
+  if n==0:
+    return l[0]
+  elif n==1:
+    return l
+  else:
+    a = 0
+    b = 1
+    for i in range(2,n):
+      c = a + b
+      l.append(c)
+      a = b
+      b = c
+  return l
 
-
+f(10)
 ----------------------------------------
 def per(n,r):
   if n>r:
