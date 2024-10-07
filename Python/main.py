fe46520bd149756bf8a115121a3e89c422605915
Rohan Das <mailtorohan.97@gmail.com>
2024-10-07 11:24:41 +0530

    Update main.py

--- Python/main.py
+++ Python/main.py
@@ -3,6 +3,24 @@
 
 
 
+------------------------------------
+def m(n):
+  l = [0,1]
+  if n==0:
+    return l[0]
+  elif n==1:
+    return l
+  else:
+    a = 0
+    b = 1
+    for i in range(2,n):
+      c = a + b
+      l.append(c)
+      a = b
+      b = c
+  return l
+
+m(10)
 ------------------------------------------------
 def f(n):
   if n==0 or n==1:
