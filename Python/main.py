6c7b77763a9e757f661946eff120f1a1531394dc
Rohan Das <mailtorohan.97@gmail.com>
2024-10-12 18:57:09 +0530

    Update main.py

--- Python/main.py
+++ Python/main.py
@@ -3,6 +3,18 @@
 
 
 
+
+-------------------------------------
+a = [1,2,3]
+b = [3,4,5]
+
+c = set(a).union(set(b))
+d = set(a).intersection(set(b))
+e = set(a).difference(set(b))
+
+print(c)
+print(d)
+print(e)
 ----------------------------
 a = np.zeros((2,3))
 a
