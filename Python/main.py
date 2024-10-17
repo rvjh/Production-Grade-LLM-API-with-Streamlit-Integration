556f006f1d5129cf66f2b9a2f18ffa1191f61e76
Rohan Das <mailtorohan.97@gmail.com>
2024-10-17 22:44:24 +0530

    Update main.py

--- Python/main.py
+++ Python/main.py
@@ -9,6 +9,15 @@
 
 
 ------------------------------
+def m(l,x):
+  for i in range(len(l)):
+    for j in range(i+1, len(l)):
+      if l[i]+l[j]==x:
+        return i,j
+  return None
+
+m([1,2,3,4],6)
+------------------------------
 def f(n):
   if n==0 or n==1:
     return 1
