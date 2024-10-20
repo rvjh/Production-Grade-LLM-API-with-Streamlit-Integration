7ebfbf56d5ca37b2a2dcfe06af96de982e94708b
Rohan Das <mailtorohan.97@gmail.com>
2024-10-20 08:56:57 +0530

    Update main.py

--- Python/main.py
+++ Python/main.py
@@ -4,8 +4,12 @@
 
 
 
+---------------------------------
+from functools import reduce
 
-
+numbers = [1, 2, 3, 4]
+result = reduce(lambda x, y: x + y, numbers)
+print(result)
 ----------------------------
 def m(a,b):
   return a*b
