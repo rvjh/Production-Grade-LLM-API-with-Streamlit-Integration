995f1c7d4fa81f8aff151bf166a6e38da4b20059
Rohan Das <mailtorohan.97@gmail.com>
2024-10-28 07:09:39 +0530

    Update main.py

--- Python/main.py
+++ Python/main.py
@@ -6,8 +6,15 @@
 
 
 
+-----------------------------------
+def m(l,x):
+  for i in range(len(l)):
+    for j in range(i+1, len(l)):
+      if l[i]+l[j]==x:
+        return i,j
+  return None
 
-
+m([1,2,3,4,5,6,7],9)
 --------------------------------------
 a = lambda x : x**2
 a(10)
