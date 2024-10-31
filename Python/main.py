dbb9f96a6f74b87057a261b6ac6ff26d66688a59
Rohan Das <mailtorohan.97@gmail.com>
2024-10-31 11:14:45 +0530

    Update main.py

--- Python/main.py
+++ Python/main.py
@@ -7,6 +7,16 @@
 
 
 
+
+----------------------------
+def m(l,x):
+  for i in range(len(l)):
+    for j in range(i+1, len(l)):
+      if l[i]+l[j]==x:
+        return i,j
+  return None
+
+m([1,2,3,4,5,6,7],9)
 ----------------------------------
 def m(a,b):
   d = {}
