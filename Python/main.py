16b0777654c045ae1e33326111e34b275bd86502
Rohan Das <mailtorohan.97@gmail.com>
2024-11-01 11:33:50 +0530

    Update main.py

--- Python/main.py
+++ Python/main.py
@@ -4,6 +4,18 @@
 
 
 
+
+-------------------------
+def m(a,b):
+  d={}
+  for i in a:
+    d[i]=True
+  for j in b:
+    if j in d:
+      return j
+    return None
+
+m([1,2,3],[3,4,5])
 ---------------------
 def per(n,r):
   if n>r:
