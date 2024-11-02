4a783e1d6384f8f20c4c4def7efed0d317618936
Rohan Das <mailtorohan.97@gmail.com>
2024-11-02 12:03:13 +0530

    Update main.py

--- Python/main.py
+++ Python/main.py
@@ -2,6 +2,14 @@
 
 
 
+---------------------------
+def m(a,b):
+  if len(a)==len(b) and a[::-1]==b[::-1]:
+    return True
+  else:
+    return False
+
+m("abcba","abcba")
 -----------------------------
 a = [1,2,3]
 b = [3,4,5]
