34d64ce5557a1044cf968427cee8f86b442cfce0
Rohan Das <mailtorohan.97@gmail.com>
2024-11-11 09:19:06 +0530

    Update main.py

--- Python/main.py
+++ Python/main.py
@@ -1,8 +1,17 @@
 
 
 
+------------------
+def m(a,b):
+  d={}
+  for i in a:
+    d[i]=True
+  for j in b:
+    if j in d:
+      return j
+  return j
 
-
+m([1,2,3],[3,4])
 ------------------------
 def m(a,b):
   a,b = b,a
