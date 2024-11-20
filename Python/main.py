4b30d71f6a0e6b0b022a5e2d2c2b30c7771ed2c4
Rohan Das <mailtorohan.97@gmail.com>
2024-11-20 07:50:54 +0530

    Update main.py

--- Python/main.py
+++ Python/main.py
@@ -2,6 +2,17 @@
 
 
 
+-------------------------
+def m(a,b):
+  d={}
+  for i in a:
+    d[i]=True
+  for j in b:
+    if j in b:
+      return j
+  return None
+
+m([1,2,3],[3,4])
 ---------------------
 def m(l,x):
   for i in range(len(l)):
