3e2dbd4b8f22592a34686b362838fd4babdd7325
Rohan Das <mailtorohan.97@gmail.com>
2024-12-02 09:41:06 +0530

    Update main.py

--- Python/main.py
+++ Python/main.py
@@ -5,6 +5,13 @@
 
 
 
+-------------------------
+def m(a,b):
+  return a*b
+
+from functools import reduce
+
+reduce(m,[1,2,3,4])
 ----------------------------
 def m(l,x):
   for i in range(len(l)):
