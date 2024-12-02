0d29096b1f655608da5b1b940ba533318cc4892e
Rohan Das <mailtorohan.97@gmail.com>
2024-12-02 09:45:06 +0530

    Update main.py

--- Python/main.py
+++ Python/main.py
@@ -4,6 +4,16 @@
 
 
 
+--------------------------
+a = [1,2,3]
+b = ['a','b','c']
+c = set(a).union(set(b))
+d = set(a).intersection(set(b))
+e = set(a).difference(set(b))
+
+print(c)
+print(d)
+print(e)
 ------------------------
 a = [1,2]
 b = ['a','b']
