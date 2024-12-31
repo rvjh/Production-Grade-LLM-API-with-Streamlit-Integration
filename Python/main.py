98796e1eb9bd02c3c66485ea624f9a67ceef8dca
Rohan Das <mailtorohan.97@gmail.com>
2024-12-31 11:53:32 +0530

    Update main.py

--- Python/main.py
+++ Python/main.py
@@ -6,6 +6,16 @@
 
 
 
+
+------------------------------
+def m(l,x):
+  for i in range(len(l)):
+    for j in range(i+1, len(l)):
+      if l[i]+l[j]==x:
+        return i,j
+  return None
+
+m([1,2,3,4],7)
 ------------------------------
 def per(n,r):
   if n>r:
