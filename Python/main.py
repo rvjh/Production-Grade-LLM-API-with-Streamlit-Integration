92dc1656e8dfad021c4bbcb767b681c4ac0fe513
Rohan Das <mailtorohan.97@gmail.com>
2025-01-02 06:12:36 +0530

    Update main.py

--- Python/main.py
+++ Python/main.py
@@ -6,6 +6,14 @@
 
 
 ----------------------
+def m(a,b):
+  if len(a)==len(b) and a[::-1]==b[::-1]:
+    return "Anagram"
+  else:
+    return "Not Anagram"
+
+m("abcba","abcba")
+----------------------
 def per(n,r):
   if n>r:
     return m(n)/m(n-r)
