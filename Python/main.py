0c46ef24835e4816d725a5bb45319cd307d9274d
Rohan Das <mailtorohan.97@gmail.com>
2025-02-08 09:34:11 +0530

    Update main.py

--- Python/main.py
+++ Python/main.py
@@ -11,6 +11,17 @@
 
 
 
+------------------------
+def m(a,b):
+  d={}
+  for i in a:
+    d[i]=True
+  for j in b:
+    if j in d:
+      return j
+  return None
+
+m([1,2,3],[3])
 ----------------------------
 def m(a,b):
   if len(a)==len(b) and a[::-1]==b[::-1]:
