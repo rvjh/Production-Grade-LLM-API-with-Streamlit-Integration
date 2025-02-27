d48b22e8c3b4dbf9aa492dc736763826b5a3170c
Rohan Das <mailtorohan.97@gmail.com>
2025-02-27 13:40:09 +0530

    Update main.py

--- Python/main.py
+++ Python/main.py
@@ -7,6 +7,17 @@
 
 
 
+
+-----------------------------------------
+WITH cte AS (
+SELECT s.user_id AS user_id, COUNT(*) tot_cnt,
+SUM(CASE WHEN action = "confirmed" THEN 1 ELSE 0 END) AS con_cnt
+FROM Signups s LEFT JOIN Confirmations c
+ON s.user_id = c.user_id
+GROUP BY s.user_id)
+
+SELECT user_id, ROUND(con_cnt / tot_cnt,2) AS confirmation_rate
+FROM cte
 ------------------------------------------
 # Write your MySQL query statement below
 
