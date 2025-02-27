bc3dc94e735a9739dbdd399986d6d9542cdca3ab
Rohan Das <mailtorohan.97@gmail.com>
2025-02-27 15:00:32 +0530

    Update main.py

--- Python/main.py
+++ Python/main.py
@@ -8,6 +8,15 @@
 
 
 
+
+------------------------------
+import pandas as pd
+
+def fix_names(users: pd.DataFrame) -> pd.DataFrame:
+    users['name'] = users['name'].str.capitalize()
+    return users.sort_values(by='user_id',ascending=True)
+    
+    
 -----------------------------
 import pandas as pd
 
