764ffb27c57b68d20f365227ab599fbcdbc622a0
Rohan Das <mailtorohan.97@gmail.com>
2025-02-27 14:51:20 +0530

    Update main.py

--- Python/main.py
+++ Python/main.py
@@ -8,6 +8,17 @@
 
 
 
+-----------------------------
+import pandas as pd
+
+def calculate_special_bonus(employees: pd.DataFrame) -> pd.DataFrame:
+    employees['bonus'] = employees.apply(
+        lambda row: row['salary'] if row['employee_id'] % 2 == 1 and not row['name'].startswith('M') else 0,
+        axis=1
+    )
+    # Sort by employee_id
+    return employees[['employee_id', 'bonus']].sort_values(by='employee_id')
+    
 ---------------------------------------
 # Write your MySQL query statement below
 
