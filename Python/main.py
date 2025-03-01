27e388788d9f31f69a4ea036245eac45d165d8af
Rohan Das <mailtorohan.97@gmail.com>
2025-03-01 20:12:04 +0530

    Update main.py

--- Python/main.py
+++ Python/main.py
@@ -10,6 +10,14 @@
 
 
 
+--------------------------
+def m(s):
+  d={}
+  for i in s:
+    d[i]=s.count(i)
+  return d
+
+m("khvkckuxuyx")
 -----------------------------
 def m(s):
   return s[::-1]
