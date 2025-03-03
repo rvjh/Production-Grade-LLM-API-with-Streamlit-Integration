d204fad849c085742a504882f0dd1ceb7f503461
Rohan Das <mailtorohan.97@gmail.com>
2025-03-03 11:38:32 +0530

    Update main.py

--- Python/main.py
+++ Python/main.py
@@ -8,6 +8,16 @@
 
 
 
+
+---------------------------------------
+# Write your MySQL query statement below
+
+
+select P.project_id, round(avg(E.experience_years),2) average_years 
+from 
+Project P left join Employee E
+on P.employee_id = E.employee_id
+group by P.project_id
 -----------------------------------
 WITH PriceSold AS (
     SELECT 
