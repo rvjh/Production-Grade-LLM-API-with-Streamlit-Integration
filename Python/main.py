5d0369783c678f299ee0b0b9e3c6864439e33bf9
Rohan Das <mailtorohan.97@gmail.com>
2025-03-07 14:41:02 +0530

    Update main.py

--- Python/main.py
+++ Python/main.py
@@ -6,6 +6,17 @@
 
 
 
+
+-------------------------------
+def per(n,r):
+  if n>r:
+    return m(n)/m(n-r)
+def com(n,r):
+  if n>r:
+    return m(n)/(m(n-r)*m(r))
+  
+print(per(5,2))
+print(com(5,2))
 ---------------------------
 def m(x):
   if x==0 or x==1:
