43c93275168759f574701547bd3befe0b8aec57c
Rohan Das <mailtorohan.97@gmail.com>
2025-03-07 14:35:35 +0530

    Update main.py

--- Python/main.py
+++ Python/main.py
@@ -1,8 +1,14 @@
 
 
 
+----------------------------
+def m(s):
+  d={}
+  for i in s:
+    d[i]=s.count(i)
+  return d
 
-
+m("qclknewnion")
 -------------------------------
 def m(s):
   return s[::-1]
