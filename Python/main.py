c1eaec73b02c7da823e023e64a893cbc56373584
Rohan Das <mailtorohan.97@gmail.com>
2025-03-09 15:28:05 +0530

    Update main.py

--- Python/main.py
+++ Python/main.py
@@ -5,9 +5,12 @@
 
 
 
+--------------------------------
+import pandas as pd
 
-
-
+def delete_duplicate_emails(person: pd.DataFrame) -> None:
+    person.sort_values(by='id', inplace=True)
+    person.drop_duplicates(subset='email', keep='first', inplace=True)
 ---------------------------------
 import pandas as pd
 
