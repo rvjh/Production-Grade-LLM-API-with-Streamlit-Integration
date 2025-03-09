aabee4b2c1662e28a62264ebdba4ad9633afe272
Rohan Das <mailtorohan.97@gmail.com>
2025-03-09 15:33:42 +0530

    Update main.py

--- Python/main.py
+++ Python/main.py
@@ -5,6 +5,20 @@
 
 
 
+
+--------------------------------------------
+import pandas as pd
+
+def rearrange_products_table(products: pd.DataFrame) -> pd.DataFrame:
+    df_long = products.melt(id_vars=['product_id'], 
+                  value_vars=['store1', 'store2', 'store3'], 
+                  var_name='store', 
+                  value_name='price')
+
+    df_long = df_long.dropna(subset=['price'])
+    return df_long
+
+    
 --------------------------------
 import pandas as pd
 
