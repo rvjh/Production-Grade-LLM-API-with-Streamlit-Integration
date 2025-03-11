f0de2c2dffd2497e9b6841d49776bece5bcdea2a
Rohan Das <mailtorohan.97@gmail.com>
2025-03-11 23:15:19 +0530

    Update main.py

--- Python/main.py
+++ Python/main.py
@@ -10,6 +10,14 @@
 
 
 
+
+--------------------------------
+a = [1,2]
+b = ['a','b']
+c = dict(zip(a,b))
+print(c)
+c.update({3:'c'})
+print(c)
 ----------------------------------
 from functools import reduce
 
