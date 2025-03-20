9e257fd41b814880e45fa6a8981d1776b854d4a3
Rohan Das <mailtorohan.97@gmail.com>
2025-03-20 20:18:08 +0530

    Update main.py

--- Python/main.py
+++ Python/main.py
@@ -10,6 +10,12 @@
 
 
 
+----------------------------------------
+select activity_date day , count(distinct user_id) active_users
+from Activity
+where activity_date between '2019-07-01' and '2019-07-27'
+group by activity_date
+order by activity_date
 
 ----------------------------------
 a = [1,2,3]
