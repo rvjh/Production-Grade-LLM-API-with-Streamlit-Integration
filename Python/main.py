30af461c0c792c6a9cad968de7e143e8f5c9bc14
Rohan Das <mailtorohan.97@gmail.com>
2025-03-20 20:31:54 +0530

    Update main.py

--- Python/main.py
+++ Python/main.py
@@ -5,7 +5,12 @@
 
 
 
+---------------------------------------------
 
+select user_id, count(follower_id) followers_count
+from Followers
+group by user_id
+order by user_id asc
 ---------------------------------------------
 
 with cte as(
