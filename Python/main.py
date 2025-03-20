4da5128b6f07e18ab68daf32b19141e035e52081
Rohan Das <mailtorohan.97@gmail.com>
2025-03-20 21:12:20 +0530

    Update main.py

--- Python/main.py
+++ Python/main.py
@@ -8,6 +8,15 @@
 
 
 
+
+------------------------------------
+import pandas as pd
+
+def count_unique_subjects(teacher: pd.DataFrame) -> pd.DataFrame:
+    df = teacher.groupby('teacher_id')['subject_id'].nunique().reset_index()
+    df.rename(columns={'subject_id':'cnt'},inplace=True)
+    return df
+    
 ---------------------------------------
 import pandas as pd
 
