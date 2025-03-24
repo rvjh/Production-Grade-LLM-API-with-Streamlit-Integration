f6a45740e71f938788730553651a884587b5b1f1
Rohan Das <mailtorohan.97@gmail.com>
2025-03-24 10:11:22 +0530

    Update main.py

--- Python/main.py
+++ Python/main.py
@@ -8,8 +8,14 @@
 
 
 
+------------------------------------
 
-
+select e1.employee_id, e1.name, count(e2.employee_id) reports_count,
+round(avg(e2.age)) average_age
+from Employees e1, Employees e2 where e1.employee_id = e2.reports_to
+group by 1
+having reports_count>0
+order by 1
 ----------------------------------------------
 select customer_id from customer 
 group by 
