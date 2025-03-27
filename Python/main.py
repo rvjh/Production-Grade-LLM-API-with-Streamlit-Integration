e7566f20140f9a2aea1d977a05d9a4c74f357656
Rohan Das <mailtorohan.97@gmail.com>
2025-03-27 11:16:39 +0530

    Update main.py

--- Python/main.py
+++ Python/main.py
@@ -5,8 +5,10 @@
 
 
 
-
-
+-----------------------------------------
+select *,
+case when x+y>z and y+z>x and z+x>y then "Yes" else "No" end as triangle
+from Triangle
 ---------------------------------------
 select employee_id,department_id
 from Employee
