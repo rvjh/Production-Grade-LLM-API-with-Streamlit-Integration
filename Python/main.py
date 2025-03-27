b0a47463bb19c99cfff32e911c339b3a2e35366b
Rohan Das <mailtorohan.97@gmail.com>
2025-03-27 11:39:42 +0530

    Update main.py

--- Python/main.py
+++ Python/main.py
@@ -4,8 +4,15 @@
 
 
 
+-----------------------------------------------------
+import pandas as pd
 
-
+def daily_leads_and_partners(daily_sales: pd.DataFrame) -> pd.DataFrame:
+    r = daily_sales.groupby(['date_id','make_name']).agg(
+        unique_leads = ('lead_id','nunique'),
+        unique_partners = ('partner_id','nunique')
+    ).reset_index()
+    return r
 -----------------------------------------
 select distinct num as ConsecutiveNums
 from(
