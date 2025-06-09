fd553eb07e18f0a01aa8676b88d1a6a4616eed2d
Rohan Das <mailtorohan.97@gmail.com>
2025-06-09 10:09:05 +0530

    Update main.py

--- Python/main.py
+++ Python/main.py
@@ -14,8 +14,13 @@
 
 
 
-
-
+----------------------
+def m(s):
+  d={}
+  for i in s:
+    d[i]=s.count(i)
+  return d
+m("dlnvwenpik")
 -----------------------------------
 def m(s):
   return s[::-1]
