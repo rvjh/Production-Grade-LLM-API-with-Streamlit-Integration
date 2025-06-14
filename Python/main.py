b20985d0be6d9e7eff9f4ed1910934cd72ec9d8b
Rohan Das <mailtorohan.97@gmail.com>
2025-06-14 10:25:54 +0530

    Update main.py

--- Python/main.py
+++ Python/main.py
@@ -23,6 +23,14 @@
 
 
 
+
+--------------------------
+def m(x):
+  if x==0 or x==1:
+    return 1
+  else:
+    return x*m(x-1)
+m(10)
 -------------------------------
 def m(l,x):
   for i in range(len(l)):
