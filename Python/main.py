807e777d67870831ccf8a4562681152cdaa3e9a8
Rohan Das <mailtorohan.97@gmail.com>
2025-06-30 10:07:31 +0530

    Update main.py

--- Python/main.py
+++ Python/main.py
@@ -23,6 +23,17 @@
 
 
 
+
+-------------------------
+def m(a,b):
+  d={}
+  for i in a:
+    d[i]=True
+  for j in b:
+    if j in d:
+      return j
+  return None
+m([1,2,3],[2,3])
 -----------------------
 def m(s):
   d={}
