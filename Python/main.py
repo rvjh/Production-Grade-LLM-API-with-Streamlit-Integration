c109aa124940e8be6fe6c9cad9ffddd9f630b0b1
Rohan Das <mailtorohan.97@gmail.com>
2025-07-19 21:18:06 +0530

    Update main.py

--- Python/main.py
+++ Python/main.py
@@ -5,6 +5,16 @@
 
 
 
+a  =[1,2,3]
+b = [2,3,4]
+c = set(a).union(set(b))
+print(c)
+d = set(a).intersection(set(b))
+print(d)
+e = set(a).difference(set(b))
+print(e)
+
+
 a = [1,2]
 b = ['a','b']
 c = dict(zip(a,b))
