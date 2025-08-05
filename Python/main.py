7d171763e1e2fcf1be508e17a7160c744c7fc9e6
Rohan Das <mailtorohan.97@gmail.com>
2025-08-05 08:21:40 +0530

    Update main.py

--- Python/main.py
+++ Python/main.py
@@ -1,4 +1,51 @@
 
+class Node:
+  def __init__(self,value):
+    self.value = value
+    self.next = None
+
+class Stack:
+  def __init__(self,value):
+    new_node = Node(value)
+    self.top = new_node
+    self.height = 1
+
+  def print_stack(self):
+    temp = self.top
+    while temp is not None:
+      print(temp.value)
+      temp = temp.next
+
+  def push(self, value):
+    new_node = Node(value)
+    if self.height == 0:
+      self.top = new_node
+    else:
+      new_node.next = self.top
+      self.top = new_node
+    self.height += 1
+
+  def pop(self):
+    if self.height == 0:
+      return None
+    else:
+      temp = self.top
+      self.top = self.top.next
+      temp.next = None
+      self.height -= 1
+      return temp
+
+a = Stack(4)
+a.print_stack()
+print("------------------")
+a.push(5)
+a.print_stack()
+print("------------------")
+a.push(6)
+a.print_stack()
+print("------------------")
+a.pop()
+a.print_stack()
 
 
 
@@ -20645,5 +20692,6 @@ print(transpose_arr)
 print(flatten_arr)
 
 -------------------------------------
+
 
 
