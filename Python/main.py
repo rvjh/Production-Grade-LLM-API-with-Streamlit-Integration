f2eaa6299e7798250fe1767e7fd27bc34161fbb2
Rohan Das <mailtorohan.97@gmail.com>
2025-08-05 08:33:56 +0530

    Update main.py

--- Python/main.py
+++ Python/main.py
@@ -1,4 +1,54 @@
+class Node:
+  def __init__(self,value):
+    self.value = value
+    self.next = None
 
+class Queue:
+  def __init__(self,value):
+    new_node = Node(value)
+    self.first = new_node
+    self.last = new_node 
+    self.length = 1
+  
+  def print_queue(self):
+    temp = self.first
+    while temp is not None:
+      print(temp.value)
+      temp = temp.next
+
+  def enqueue(self, value):
+    new_node = Node(value)
+    if self.first is None:
+      self.first = new_node
+      self.last = new_node
+    else:
+      self.last.next = new_node
+      self.last
+    self.length += 1
+
+  def dequeue(self):
+    if self.length == 0:
+      return None
+    else:
+      temp = self.first
+      self.first = self.first.next
+      temp.next = None
+      self.length -= 1
+      if self.length == 0:
+        self.last = None
+      return temp
+
+b = Queue(2)
+b.print_queue()
+print("------------------")
+b.enqueue(3)
+b.print_queue()
+print("------------------")
+b.enqueue(4)
+b.print_queue()
+print("------------------")
+b.dequeue()
+b.print_queue()
 
 
 class Node:
@@ -20717,6 +20767,7 @@ print(transpose_arr)
 print(flatten_arr)
 
 -------------------------------------
+
 
 
 
