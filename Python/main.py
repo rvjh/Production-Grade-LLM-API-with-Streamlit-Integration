e5277246a44e63e1cddcfec3f2f044b6d14de6fc
Rohan Das <mailtorohan.97@gmail.com>
2025-08-18 09:54:28 +0530

    Update main.py

--- Python/main.py
+++ Python/main.py
@@ -1,6 +1,11 @@
 
 
-
+a = [1,2]
+b = ['a','b']
+c = dict(zip(a,b))
+print(c)
+c.update({3:'c'})
+print(c)
 
 from functools import reduce
 reduce(a,[1,2,3,4])
@@ -21171,6 +21176,7 @@ print(transpose_arr)
 print(flatten_arr)
 
 -------------------------------------
+
 
 
 
