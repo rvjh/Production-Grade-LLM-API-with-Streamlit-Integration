da7caf8d6be5004e0019d6be2fb6a11c93da70c3
Rohan Das <mailtorohan.97@gmail.com>
2025-08-18 09:59:28 +0530

    Update main.py

--- Python/main.py
+++ Python/main.py
@@ -1,6 +1,23 @@
 
 
+import numpy as np
 
+def mat_mul(a,b):
+  col_a = a.shape[1]
+  row_b = b.shape[0]
+  if col_a != row_b:
+    return "mot possndc"
+  else:
+    r = np.zeros((col_a, row_b))
+    for i in range(col_a):
+      for j in range(row_b):
+        for k in range(col_a):
+          r[i][j] = r[i][j] + a[i][k]*b[k][j]
+    return r
+
+a = np.array([[1,2],[3,4]])
+b = np.array([[5,6],[7,8]])
+mat_mul(a,b)
 
 a = [1,2,3]
 b = [3,4,5]
@@ -21187,6 +21204,7 @@ print(transpose_arr)
 print(flatten_arr)
 
 -------------------------------------
+
 
 
 
