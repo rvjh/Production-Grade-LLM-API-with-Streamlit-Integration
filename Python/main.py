e590644849bcf264f73eb99999a23a4c4ac0f74d
Rohan Das <mailtorohan.97@gmail.com>
2025-09-13 11:21:22 +0530

    Update main.py

--- Python/main.py
+++ Python/main.py
@@ -1,5 +1,31 @@
 
 
+
+
+import numpy as np
+
+def mat_mul(a,b):
+  col_a = a.shape[1]
+  row_b = b.shape[0]
+  if col_a!= row_b:
+    return "not possible"
+  else:
+    r = np.zeros((col_a,row_b))
+    for i in range(col_a):
+      for j in range(row_b):
+        for k in range(col_a):
+          r[i][j] = r[i][j] + a[i][k]*b[k][j]
+    return r
+a = np.array([
+    [1,2],[2,3]
+])
+
+b = np.array([
+    [1,2],[2,3]
+])
+
+mat_mul(a,b)
+
 a = [1,2,3]
 b = [2,3,4]
 c = set(a).union(set(b))
@@ -21750,6 +21776,7 @@ print(transpose_arr)
 print(flatten_arr)
 
 -------------------------------------
+
 
 
 
