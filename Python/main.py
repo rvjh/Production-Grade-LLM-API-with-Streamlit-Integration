70d8e6d062d232a68b4a11a06109ac1400e9d17a
Rohan Das <mailtorohan.97@gmail.com>
2025-09-16 09:07:20 +0530

    Update main.py

--- Python/main.py
+++ Python/main.py
@@ -1,7 +1,12 @@
 
 
 
-
+def m(s):
+  d={}
+  for i in s:
+    d[i]=s.count(i)
+  return d
+m("jwdncwjn")
 
 def m(s):
   return s[::-1]
@@ -21781,6 +21786,7 @@ print(transpose_arr)
 print(flatten_arr)
 
 -------------------------------------
+
 
 
 
