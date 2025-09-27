c32095c17ec6bd9e0eb587694b6d1547bd41a138
Rohan Das <mailtorohan.97@gmail.com>
2025-09-27 09:34:55 +0530

    Update main.py

--- Python/main.py
+++ Python/main.py
@@ -1,6 +1,11 @@
 
 
-
+def m(x):
+  if x==0 or x==1:
+    return 1
+  else:
+    return x*m(x-1)
+m(10)
 
 def m(l,x):
   d={}
@@ -22154,6 +22159,7 @@ print(transpose_arr)
 print(flatten_arr)
 
 -------------------------------------
+
 
 
 
