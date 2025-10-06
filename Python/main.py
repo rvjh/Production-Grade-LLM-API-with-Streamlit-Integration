6bfe86ec95c39988521613d20a0b1c4d57476a95
Rohan Das <mailtorohan.97@gmail.com>
2025-10-06 14:58:20 +0530

    Update main.py

--- Python/main.py
+++ Python/main.py
@@ -1,6 +1,15 @@
 
 
 
+def per(n,r):
+  if n>r:
+    return m(n)/(m(n-r))
+def com(n,r):
+  if n>r:
+    return m(n)/(m(r)*m(n-r))
+print(per(5,2))
+print(com(5,2))
+
 
 def m(x):
   if x==0 or x==1:
@@ -22440,6 +22449,7 @@ print(transpose_arr)
 print(flatten_arr)
 
 -------------------------------------
+
 
 
 
