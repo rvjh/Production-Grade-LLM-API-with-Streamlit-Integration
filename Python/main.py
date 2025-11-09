591d54f4a1ccb4840063e3d12b36ea9ee7420b00
Rohan Das <mailtorohan.97@gmail.com>
2025-11-09 09:12:35 +0530

    Update main.py

--- Python/main.py
+++ Python/main.py
@@ -1,4 +1,8 @@
 
+
+a = lambda x,y : x*y
+a(10,2)
+
 def m(x):
   l=[0,1]
   if x==0:
@@ -22996,6 +23000,7 @@ print(transpose_arr)
 print(flatten_arr)
 
 -------------------------------------
+
 
 
 
