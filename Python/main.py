d20b1616d42c337ac4653b5d6e33fd3718241a5a
Rohan Das <mailtorohan.97@gmail.com>
2025-11-09 09:13:07 +0530

    Update main.py

--- Python/main.py
+++ Python/main.py
@@ -1,5 +1,8 @@
 
 
+
+list(map(a,[1,2,3],[2,3,4]))
+
 a = lambda x,y : x*y
 a(10,2)
 
@@ -23000,6 +23003,7 @@ print(transpose_arr)
 print(flatten_arr)
 
 -------------------------------------
+
 
 
 
