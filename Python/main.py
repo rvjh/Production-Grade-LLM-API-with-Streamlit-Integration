ee7217eb540d61e56b4200a58c6d0f9333872c14
Rohan Das <mailtorohan.97@gmail.com>
2025-11-20 09:27:28 +0530

    Update main.py

--- Python/main.py
+++ Python/main.py
@@ -1,5 +1,15 @@
 
 
+a = [1,2,3]
+b = [3,4,5]
+c = set(a).union(set(b))
+d = set(a).intersection(set(b))
+e = set(a).difference(set(b))
+
+print(c)
+print(e)
+print(d)
+
 a = [1,2]
 b = ['a','b']
 c = dict(zip(a,b))
@@ -23135,6 +23145,7 @@ print(transpose_arr)
 print(flatten_arr)
 
 -------------------------------------
+
 
 
 
