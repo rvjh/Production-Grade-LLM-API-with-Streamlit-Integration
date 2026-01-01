6b90ef6eb72ec6f318e1d4a67f8f01fcfb868cc8
Rohan Das <mailtorohan.97@gmail.com>
2026-01-01 08:39:42 +0530

    Update main.py

--- Python/main.py
+++ Python/main.py
@@ -1,4 +1,15 @@
 
+
+def m(a,b):
+  d={}
+  for i in a:
+    d[i]=True
+  for j in b:
+    if j in d:
+      return j
+  return None
+m([1,2,3,3],[2,3])
+
 def m(s):
   d={}
   for i in s:
@@ -23220,6 +23231,7 @@ print(transpose_arr)
 print(flatten_arr)
 
 -------------------------------------
+
 
 
 
