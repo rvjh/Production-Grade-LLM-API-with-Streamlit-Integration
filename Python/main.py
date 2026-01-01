e73ff79e5464ff99c69c51a49b411dae897d89ed
Rohan Das <mailtorohan.97@gmail.com>
2026-01-01 08:50:52 +0530

    Update main.py

--- Python/main.py
+++ Python/main.py
@@ -1,5 +1,13 @@
 
 
+a = [1,2,3]
+b = [3,4,5]
+c = set(a).union(set(b))
+d = set(a).intersection(set(b))
+e = set(a).difference(set(b))
+print(c)
+print(d)
+print(e)
 
 
 a = [1,2]
@@ -23289,6 +23297,7 @@ print(transpose_arr)
 print(flatten_arr)
 
 -------------------------------------
+
 
 
 
