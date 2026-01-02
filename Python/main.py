be6127759edae49e48cef2f52920d2be1a6910d9
Rohan Das <mailtorohan.97@gmail.com>
2026-01-02 23:44:42 +0530

    Update main.py

--- Python/main.py
+++ Python/main.py
@@ -1,5 +1,17 @@
 
 
+
+def m(l,x):
+  d={}
+  for i in l:
+    diff = x - l[i]
+    if diff in d:
+      return [d[diff],i]
+    else:
+      d[l[i]] = i
+  return d
+m([1,2,3],5)
+
 def m(a,b):
   d={}
   for i in a:
@@ -23429,6 +23441,7 @@ print(transpose_arr)
 print(flatten_arr)
 
 -------------------------------------
+
 
 
 
