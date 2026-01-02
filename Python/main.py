f7b532b65269706ab0a5fc1beb21e28fe8f047e3
Rohan Das <mailtorohan.97@gmail.com>
2026-01-02 23:41:09 +0530

    Update main.py

--- Python/main.py
+++ Python/main.py
@@ -1,5 +1,22 @@
 
 
+SELECT 
+    *,
+    SUBSTRING_INDEX(filter_room_types, ',', 1) AS room_type_1, 
+    SUBSTRING_INDEX(SUBSTRING_INDEX(filter_room_types, ',', 2), ',', -1) AS room_type_2
+FROM airbnb_searches;
+
+select room, count(*) from 
+(select *,
+SUBSTRING_INDEX(filter_room_types, ',', 1) room
+from airbnb_searches
+union all
+select *,
+SUBSTRING_INDEX(SUBSTRING_INDEX(filter_room_types, ',', 2), ',', -1) room
+from airbnb_searches) A
+group by room
+order by count(*) desc;
+
 
 with cte as(
 SELECT emp_id,
@@ -23390,6 +23407,7 @@ print(transpose_arr)
 print(flatten_arr)
 
 -------------------------------------
+
 
 
 
