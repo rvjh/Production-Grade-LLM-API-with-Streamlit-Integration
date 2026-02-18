c81f6289c178f241179f0320ca333e488bc247e2
Rohan Das <mailtorohan.97@gmail.com>
2026-02-18 08:33:35 +0530

    Refactor and add permutation/combinations functions

--- RAG/Basic_Rag.py
+++ RAG/Basic_Rag.py
@@ -1,5 +1,14 @@
 
 
+def per(n,r):
+  if n>r:
+    return m(n)/m(n-r)
+def com(n,r):
+  if n>r:
+    return m(n)/(m(r)*m(n-r))
+
+print(per(5,2))
+print(com(5,2))
 
 def m(x):
   l=[0,1]
@@ -110,6 +119,7 @@ db = Chroma(documents[:], OllamaEmbeddings())
 query = "Who are the authors of attention is all you need?"
 retireved_results=db.similarity_search(query)
 print(retireved_results[0].page_content)
+
 
 
 
