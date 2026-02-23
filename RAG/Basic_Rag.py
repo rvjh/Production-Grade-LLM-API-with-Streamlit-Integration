73a5c139a15c9fc72f86112e9c7349eab3811442
Rohan Das <mailtorohan.97@gmail.com>
2026-02-23 08:54:07 +0530

    Update Basic_Rag.py

--- RAG/Basic_Rag.py
+++ RAG/Basic_Rag.py
@@ -1,5 +1,28 @@
 
+import numpy as np
 
+def mat_mul(a,b):
+  col_a = a.shape[1]
+  row_b = b.shape[0]
+  if col_a != row_b:
+    return "not possible"
+  else:
+    r = np.zeros((col_a, row_b))
+    for i in range(col_a):
+      for j in range(row_b):
+        for k in range(col_a):
+          r[i][j] = r[i][j] + a[i][k]*b[k][j]
+    return r
+
+a = np.array([
+    [1,2],[2,3]
+])
+
+b = np.array([
+    [1,2],[2,3]
+])
+
+mat_mul(a,b)
 
 a = [1,2,3]
 b = [3,4,5]
@@ -875,6 +898,7 @@ db = Chroma(documents[:], OllamaEmbeddings())
 query = "Who are the authors of attention is all you need?"
 retireved_results=db.similarity_search(query)
 print(retireved_results[0].page_content)
+
 
 
 
