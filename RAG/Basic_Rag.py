33e0e958fc537c8b136e380fdb6e45e6094a5e66
Rohan Das <mailtorohan.97@gmail.com>
2026-02-25 08:51:09 +0530

    Update Basic_Rag.py

--- RAG/Basic_Rag.py
+++ RAG/Basic_Rag.py
@@ -1,5 +1,22 @@
 
 
+
+def m(x):
+  l=[0,1]
+  if x==0:
+    return l[0]
+  elif x==1:
+    return l
+  else:
+    a,b = 0,1
+    for i in range(2,x):
+      c = a + b
+      l.append(c)
+      a = b
+      b = c
+  return l
+m(10)
+
 def per(n,r):
   if n>r:
     return m(n)/m(n-r)
@@ -1148,6 +1165,7 @@ db = Chroma(documents[:], OllamaEmbeddings())
 query = "Who are the authors of attention is all you need?"
 retireved_results=db.similarity_search(query)
 print(retireved_results[0].page_content)
+
 
 
 
