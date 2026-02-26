edaff20aba9c58c0c13ed6bd93ad0eea9c3eb3d1
Rohan Das <mailtorohan.97@gmail.com>
2026-02-26 08:22:26 +0530

    Update Basic_Rag.py

--- RAG/Basic_Rag.py
+++ RAG/Basic_Rag.py
@@ -2,7 +2,9 @@
 
 
 
+from functools import reduce
 
+reduce(a,[1,2,3])
 
 a = lambda x,y : x*y
 a(101,2)
@@ -1301,6 +1303,7 @@ db = Chroma(documents[:], OllamaEmbeddings())
 query = "Who are the authors of attention is all you need?"
 retireved_results=db.similarity_search(query)
 print(retireved_results[0].page_content)
+
 
 
 
