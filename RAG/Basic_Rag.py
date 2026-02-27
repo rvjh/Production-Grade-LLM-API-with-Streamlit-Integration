7ec78a64b11f9d62e837dcc2617ed0a57ee9869e
Rohan Das <mailtorohan.97@gmail.com>
2026-02-27 09:44:02 +0530

    Update Basic_Rag.py

--- RAG/Basic_Rag.py
+++ RAG/Basic_Rag.py
@@ -1,7 +1,9 @@
 
 
 
-
+def m(s):
+  return s[::-1]
+m("alcklk")
 
 select * from billings;
 select * from hoursworked;
@@ -1373,6 +1375,7 @@ db = Chroma(documents[:], OllamaEmbeddings())
 query = "Who are the authors of attention is all you need?"
 retireved_results=db.similarity_search(query)
 print(retireved_results[0].page_content)
+
 
 
 
